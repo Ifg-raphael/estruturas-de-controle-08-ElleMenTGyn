74be9d05d40f1de02212e1e80f5c0dc8612e1e3f
ElleMenTGyn <77647739+ElleMenTGyn@users.noreply.github.com>
2025-08-26 15:09:22 -0300

    Update calcula_peso_ideal.py
    Zerado

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,10 +1,1 @@
-genero = input("Digite o genero(M/F):")
 
-h = (input("Digite a altura:"))
-
-if genero == "F":
-   peso = float((62.1*h)-44.7)
-else:
-   peso = float((72.7*h)-58)
-
-print(f"Seu peso ideal é {peso:.2f}")
