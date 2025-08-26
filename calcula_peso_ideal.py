1f21dfdd516096f4a21982006a69dcc0422fed87
ElleMenTGyn <77647739+ElleMenTGyn@users.noreply.github.com>
2025-08-26 14:23:04 -0300

    Update calcula_peso_ideal.py
    Segue codigo que calcula peso ideal, baseado no genero do usuario, e tb da altura.

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,1 +1,10 @@
-# Sua solução aqui
+genero = input("Digite o genero(M/F):")
+
+h = float(input("Digite a altura:"))
+
+if genero == "F":
+   peso = float((62.1*h)-44.7)
+else:
+   peso = float((72.7*h)-58)
+
+print(f"Seu peso ideal é {peso:.2f}")
